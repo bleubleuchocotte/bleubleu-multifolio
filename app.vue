869a78b3219044ebcd6ef2b960f35a892ac92372
Thomas <thomas.auffroy@gmail.com>
2023-04-25 14:27:07 +0200

    Ajout de composant

--- app.vue
+++ app.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="body">
     <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
   </div>
 </template>
 
 <style scoped lang="scss">
 .body {
+  display: flex;
+  flex-direction: column;
+
+  gap: 10px;
+
+  height: 100vh;
+  width: 100vw;
   padding-inline: $body-padding-inline;
+  padding-block: $body-padding-block;
 }
 </style>
