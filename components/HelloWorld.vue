fd82005d9a885d38d14236b543385e5af9ee6c7c
Thomas <thomas.auffroy@gmail.com>
2023-04-25 10:31:23 +0200

    init

---
+++ components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const count = ref(1)
+
+watch(count, () => {
+	console.log("oui")
+})
+</script>
+
+<template>
+
+	<button @click="count += 1">{{ count }}</button>
+</template>
