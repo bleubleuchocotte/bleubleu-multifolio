ec2bcc407b79a3caf2dea76fdb9727d9d28b6de0
Thomas <thomas.auffroy@gmail.com>
2023-04-25 10:58:10 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/main.scss";',
+        },
+      },
+    },
+  },
+});
