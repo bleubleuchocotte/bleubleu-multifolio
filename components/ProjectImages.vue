5f0570ffae3477fa4d40965c1f2820d47665b7ab
Thomas <thomas.auffroy@gmail.com>
2023-04-26 14:28:10 +0200

    Ajout d'un helper pour scroll

--- components/ProjectImages.vue
+++ components/ProjectImages.vue
@@ -11,17 +11,51 @@ defineProps({
 </script>
 
 <template>
-  <BoxScrollable :title="''">
-    <img
-      v-for="(image, index) in project.images"
-      :key="index"
-      :src="image.url"
-      alt=""
-    />
-  </BoxScrollable>
+  <div style="position: relative">
+    <BoxScrollable :title="''" class="pb-0">
+      <img
+        v-for="(image, index) in project.images"
+        :key="index"
+        :src="image.url"
+        alt=""
+      />
+    </BoxScrollable>
+
+    <div class="help">
+      <p>
+        Don't forget to scroll <i><IconArrowDown /></i>
+      </p>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.pb-0 {
+  padding-bottom: 0;
+}
+.help {
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+
+  width: 100%;
+
+  p {
+    display: flex;
+    gap: 15px;
+    width: fit-content;
+
+    text-align: center;
+    text-transform: uppercase;
+    color: $background-color;
+
+    padding: 10px;
+    margin: 0 auto;
+
+    border-radius: 12px 12px 0 0;
+    background-color: $text-color;
+  }
+}
 img {
   border-radius: 12px;
   width: 100%;
