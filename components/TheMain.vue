ac6bbc6ff207b1cab4dd98b96406e733598001d7
Thomas <thomas.auffroy@gmail.com>
2023-06-26 15:13:04 +0200

    Début de la strucuture

--- components/TheMain.vue
+++ components/TheMain.vue
@@ -9,5 +9,27 @@ defineProps({
 </script>
 
 <template>
-  <main>{{ params }}</main>
+  <main class="main">
+    <div class="main__left">
+      <div>About</div>
+      <div>Liste des projets</div>
+    </div>
+    <UIBaseSeparator :width="1" />
+    <div class="main__right">Liste des projets scrollable</div>
+  </main>
 </template>
+
+<style scoped lang="scss">
+.main {
+  flex: 1;
+  display: flex;
+
+  &__left {
+    @include left;
+  }
+
+  &__right {
+    @include right;
+  }
+}
+</style>
