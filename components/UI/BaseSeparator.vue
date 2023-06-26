b13306fd22a2ff1a11deb9e3bb88d86f1449abb1
Thomas <thomas.auffroy@gmail.com>
2023-06-26 15:12:55 +0200

    create

---
+++ components/UI/BaseSeparator.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  width: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+});
+
+const styles = reactive({
+  "--width": `${props.width}px`,
+});
+</script>
+
+<template>
+  <div :style="styles"></div>
+</template>
+
+<style scoped lang="scss">
+div {
+  height: 100%;
+  width: var(--width);
+
+  background-color: var(--border-color);
+}
+</style>
