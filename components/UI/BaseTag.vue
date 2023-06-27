5864e0ca07ef755eae77d3b21e188c7007ecd420
Thomas <thomas.auffroy@gmail.com>
2023-06-27 15:28:49 +0200

    Création du composant

---
+++ components/UI/BaseTag.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="tag__text"><slot></slot></span>
+</template>
+
+<style scoped lang="scss">
+.tag {
+  &__text {
+    padding: 10px;
+    border: 1px solid var(--border-color);
+    border-radius: $border-radius;
+  }
+}
+</style>
