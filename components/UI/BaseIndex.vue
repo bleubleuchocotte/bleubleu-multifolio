252f929378b32a74bef70a33cffeaec2658b2e37
Thomas <thomas.auffroy@gmail.com>
2023-06-27 12:01:00 +0200

    Création d'un composantn index

---
+++ components/UI/BaseIndex.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  index: {
+    type: Number,
+    required: true,
+  },
+});
+
+const i = ref(props.index < 10 ? `0${props.index}` : props.index.toString());
+</script>
+
+<template>
+  <span>{{ i }}</span>
+</template>
