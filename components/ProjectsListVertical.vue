b582e0fa70c82508580261ad8fee9e45c03b69ef
Thomas <thomas.auffroy@gmail.com>
2023-06-27 12:01:40 +0200

    Création de la structure

--- components/ProjectsListVertical.vue
+++ components/ProjectsListVertical.vue
@@ -1,7 +1,55 @@
 <script setup lang="ts">
-// La liste de tous les projets
+import { Project } from "~/type/types";
+
+defineProps({
+  projects: {
+    type: Array<Project>,
+    required: true,
+  },
+});
 </script>
 
 <template>
-  <div>oui</div>
+  <ul class="projects-list-vertical">
+    <li
+      v-for="(project, i) in projects"
+      :key="i"
+      class="projects-list-vertical__element"
+    >
+      <h2 class="projects-list-vertical__element-left">
+        <UIBaseIndex :index="i + 1" />{{ project.title }}
+      </h2>
+      <div class="projects-list-vertical__element-right">
+        <p>{{ project.date.slice(0, 4) }}</p>
+        <p>{{ project.skills[0].skill }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
+
+<style scoped lang="scss">
+.projects-list-vertical {
+  padding-left: 5px;
+  padding-top: $gutter;
+  &__element {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid var(--border-color);
+
+    padding-block: calc($gutter / 2);
+
+    &-left {
+      text-transform: uppercase;
+
+      span {
+        display: inline-block;
+        width: calc($gutter * 2);
+      }
+    }
+
+    &-right {
+      text-align: right;
+    }
+  }
+}
+</style>
