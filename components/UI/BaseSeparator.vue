16132d044a2458b5b0d9a8406963dd1ac103440d
Thomas <thomas.auffroy@gmail.com>
2023-06-30 21:13:04 +0200

    Shadow effect on scrollable box

--- components/UI/BaseSeparator.vue
+++ components/UI/BaseSeparator.vue
@@ -18,9 +18,27 @@ const styles = reactive({
 
 <style scoped lang="scss">
 div {
+  position: relative;
+
   height: 100%;
   width: var(--width);
 
   background-color: var(--border-color);
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 1px;
+    width: calc($gutter + $gutter / 2);
+    height: 100%;
+    background: linear-gradient(
+      270deg,
+      hsla(0, 0%, 100%, 0),
+      var(--background-color)
+    );
+    z-index: 1;
+    pointer-events: none;
+    backdrop-filter: opacity(100%);
+  }
 }
 </style>
