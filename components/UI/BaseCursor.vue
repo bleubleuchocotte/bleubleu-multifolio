d21118c7e5aee80d1e043dab4b59dc9b11253a9d
Thomas <thomas.auffroy@gmail.com>
2023-07-03 10:57:41 +0200

    Create file

---
+++ components/UI/BaseCursor.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="cursor__container"></div>
+</template>
+
+<style scoped lang="scss">
+.cursor {
+  &__container {
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+  }
+}
+</style>
