992c39c23b8eef1ac18799fdd7ec72e3462c8535
Thomas <thomas.auffroy@gmail.com>
2023-07-17 11:59:12 +0200

    Add logic to Transition

--- app.vue
+++ app.vue
@@ -51,6 +51,16 @@ body {
   font-family: "Manrope";
   overflow: hidden;
 }
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
 </style>
 
 <style scoped lang="scss">
