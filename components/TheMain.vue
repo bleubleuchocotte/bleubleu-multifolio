ffe90d68945e67a27becbda0d99e3ecd1c5fcc97
Thomas <thomas.auffroy@gmail.com>
2023-07-17 15:41:45 +0200

    Add transition to gallery

--- components/TheMain.vue
+++ components/TheMain.vue
@@ -50,7 +50,7 @@ const projectInGallery = ref<Project | null>(null);
       />
     </UIBaseLenis>
 
-    <Transition>
+    <Transition name="slide-fade">
       <TheGallery
         v-if="projectInGallery != null"
         :project="projectInGallery"
@@ -97,5 +97,18 @@ const projectInGallery = ref<Project | null>(null);
       pointer-events: none;
     }
   }
+}
+</style>
+
+<style>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
 }
 </style>
