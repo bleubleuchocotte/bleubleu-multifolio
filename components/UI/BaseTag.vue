016f09bf1ad9e2ec48e52c653ca65ff9a433e8e7
Alexandre Bottollier <alexandre.bottollier@gmail.com>
2023-10-09 11:17:08 +0200

    Rework of the desktop about section + correct basetag padding

--- components/UI/BaseTag.vue
+++ components/UI/BaseTag.vue
@@ -5,9 +5,9 @@
 <style scoped lang="scss">
 .tag {
   &__text {
-    @include padding(0.5);
+    @include padding(0.33);
     border: 1px solid var(--border-color);
-    @include border-radius();
+    @include border-radius(0.5);
 
     @include font("p");
   }
