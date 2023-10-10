9cd8157b346eb1925e8eaf576406c8767ef4aad9
Alexandre Bottollier <alexandre.bottollier@gmail.com>
2023-10-10 20:15:13 +0200

    Introduce 404 page

---
+++ components/UI/BaseButtonHome.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtLink to="/" class="button">
+    <div class="button__text">
+      <div class="button__text-bg-top">
+        <span><slot /></span>
+      </div>
+      <div class="button__text-bg-bottom">
+        <span><slot /></span>
+      </div>
+    </div>
+
+    <div class="button__reveal">
+      <span>⏎</span>
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped lang="scss">
+.button {
+  @include font("cta");
+
+  position: relative;
+  overflow: hidden;
+
+  height: 100%;
+  width: fit-content;
+
+  @include border-radius(0.5);
+
+  color: var(--text-accent-color);
+
+  &:hover .button__text-bg,
+  &.open .button__text-bg {
+    &-top {
+      transform: translate3d(0, -100%, 0);
+    }
+    &-bottom {
+      transform: translate3d(0, 100%, 0);
+    }
+  }
+
+  &__text {
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    top: 0;
+    z-index: 1;
+
+    &-bg-top,
+    &-bg-bottom {
+      width: 100%;
+      height: 50%;
+
+      background-color: var(--accent-color);
+
+      position: relative;
+
+      overflow: hidden;
+      transition: transform 0.3s ease-out;
+
+      span {
+        width: 100%;
+        position: absolute;
+
+        left: 0;
+
+        text-align: center;
+      }
+    }
+
+    &-bg-top {
+      span {
+        bottom: 0;
+
+        transform: translate3d(0, 50%, 0);
+      }
+    }
+
+    &-bg-bottom {
+      span {
+        top: 0;
+
+        transform: translate3d(0, -50%, 0);
+      }
+    }
+  }
+
+  &__reveal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100%;
+    width: 100%;
+
+    position: absolute;
+    left: 0;
+    top: 0;
+
+    background-color: var(--background-color);
+    color: var(--accent-color);
+
+    @include border(0.5, var(--accent-color));
+  }
+}
+</style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,63 +1,81 @@
 <template>
-  <div class="glitch" data-text="404">404</div>
+  <div class="container">
+    <div class="glitch" data-text="404">404</div>
+    <UIBaseButtonHome class="button">Let's go home</UIBaseButtonHome>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.container {
+  position: relative;
+  display: flex;
+}
+.button {
+  height: 7vw;
+  width: 20vw;
+  z-index: 0;
+  border: 1px solid var(--background-color);
+  position: absolute;
+  top: calc(24vw - 7vw);
+  left: calc(50vw - 10vw);
+  @include font("h2");
+
+  &:deep(.button__reveal) {
+    border-radius: 0;
+    border: unset;
+  }
+}
 .glitch {
   color: var(--text-color);
-  font-size: 90vh;
-  // position: relative;
-  // line-height: 1;
-  // margin: auto;
-  // font-weight: 900;
-  // transform: skewx(-30deg);
-  // letter-spacing: -12vw;
-  // width: 100vw;
-  // height: 100vh;
+  font-size: 45vw;
+  position: relative;
+  line-height: 0.9;
+  margin: auto;
+  font-weight: 900;
+  transform: skewX(-30deg);
+  z-index: -5;
 }
-// @keyframes noise-anim {
-//   $steps: 20;
-//   @for $i from 0 through $steps {
-//     #{percentage($i * calc(1/$steps))} {
-//       clip: rect(random(500) + px, 9999px, random(500) + px, 0);
-//     }
-//   }
-// }
-// .glitch:after {
-//   content: attr(data-text);
-//   position: absolute;
-//   left: 20px;
-//   text-shadow: -1px 0 var(--accent-color);
-//   top: -40vh;
-//   color: var(--text-color);
-//   background: var(--background-color);
-//   overflow: hidden;
-//   clip: rect(0, 9000px, 0, 0);
-//   animation: noise-anim 2s infinite linear alternate-reverse;
-//   height: 50vh;
-//   width: 100vw;
-// }
+@keyframes noise-anim {
+  $steps: 20;
+  @for $i from 0 through $steps {
+    #{percentage($i * calc(1/$steps))} {
+      clip: rect(random(800) + px, 9999px, random(800) + px, 0);
+    }
+  }
+}
+.glitch:after {
+  content: attr(data-text);
+  top: 0;
+  left: 5px;
+  position: absolute;
+  text-shadow: -10px 0 var(--accent-color);
+  color: var(--text-color);
+  background: var(--background-color);
+  overflow: hidden;
+  clip: rect(0, 900px, 0, 0);
+  animation: noise-anim 2s infinite linear alternate-reverse;
+  height: 100vh;
+}
 
-// @keyframes noise-anim-2 {
-//   $steps: 20;
-//   @for $i from 0 through $steps {
-//     #{percentage($i * calc(1/$steps))} {
-//       clip: rect(random(500) + px, 9999px, random(500) + px, 0);
-//     }
-//   }
-// }
-// .glitch:before {
-//   content: attr(data-text);
-//   position: absolute;
-//   left: -20px;
-//   text-shadow: 1px 0 var(--text-color);
-//   top: -40vh;
-//   color: var(--text-color);
-//   background: var(--background-color);
-//   overflow: hidden;
-//   clip: rect(0, 9000px, 0, 0);
-//   animation: noise-anim-2 3s infinite linear alternate-reverse;
-//   height: 50vh;
-//   width: 100vw;
-// }
+@keyframes noise-anim-2 {
+  $steps: 20;
+  @for $i from 0 through $steps {
+    #{percentage($i * calc(1/$steps))} {
+      clip: rect(random(800) + px, 9999px, random(800) + px, 0);
+    }
+  }
+}
+.glitch:before {
+  content: attr(data-text);
+  position: absolute;
+  top: 0;
+  left: -5px;
+  text-shadow: 10px 0 var(--accent-color);
+  color: var(--text-color);
+  background: var(--background-color);
+  overflow: hidden;
+  clip: rect(0, 900px, 0, 0);
+  animation: noise-anim-2 3s infinite linear alternate-reverse;
+  height: 100vh;
+}
 </style>
