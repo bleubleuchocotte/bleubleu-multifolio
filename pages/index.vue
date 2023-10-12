51cf1b76c102ae67cbc479393c2ab3b81813d591
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-10-12 17:12:48 +0200

    Match server rendered component with device module

--- pages/index.vue
+++ pages/index.vue
@@ -37,18 +37,40 @@ useServerSeoMeta({
   publisher: `${website.me["first-name"]} ${website.me["last-name"]}`,
 });
 
-const isMobile = useMediaQuery("(max-width: 1025px)");
+const isMobile = useMediaQuery("(max-width: 1025px)"); // Client side
+const { isDesktop } = useDevice(); // Server side
 </script>
 
 <template>
   <div class="index-page">
-    <TheMainMobile v-show="isMobile" :projects="projects" :website="website" />
-    <TheMain v-show="!isMobile" :projects="projects" :website="website" />
+    <ClientOnly>
+      <TheMainMobile v-if="isMobile" :projects="projects" :website="website" />
+      <TheMain
+        v-else
+        class="index-page__desktop"
+        :projects="projects"
+        :website="website"
+      />
+
+      <template #fallback>
+        <TheMain
+          v-if="isDesktop"
+          :projects="projects"
+          :website="website"
+          class="index-page__desktop"
+        />
+        <TheMainMobile v-else :projects="projects" :website="website" />
+      </template>
+    </ClientOnly>
   </div>
 </template>
 
 <style scoped lang="scss">
 .index-page {
   min-height: 0;
+
+  &__desktop {
+    height: 100%;
+  }
 }
 </style>
