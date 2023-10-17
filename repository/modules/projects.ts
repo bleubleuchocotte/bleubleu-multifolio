e50ae6487ff26332a78ae989f9d98c775e1eca4e
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-10-17 17:59:18 +0200

    Handle case where project could be empty prismic side

--- repository/modules/projects.ts
+++ repository/modules/projects.ts
@@ -47,31 +47,37 @@ class ProjectsModule {
       throw new Error("Project could not be loaded");
     }
 
-    const projects: Project[] = prismicProjects.value.data.projects.map(
-      (el: { project: PrismicDocument }) => {
-        const result: Project = {
-          id: el.project.id,
-          date: el.project.data.date,
-          images: el.project.data.images.map(
-            (img: { image: PrismicDocument }) => {
-              return {
-                field: img.image.data,
-                type: img.image.type,
-              };
-            }
-          ),
-          description: el.project.data.description,
-          title: el.project.data.title,
-          skills: el.project.data.skills,
-          "image-mobile": el.project.data["image-mobile"],
-          url: el.project.data.url.url ? el.project.data.url : null,
-        };
+    const projects: Array<Project | null> =
+      prismicProjects.value.data.projects.map(
+        (el: { project: PrismicDocument }) => {
+          if (!el.project.data) {
+            // Si un champ n'a pas été remplie, renvoi null
+            return null;
+          }
 
-        return result;
-      }
-    );
+          const result: Project = {
+            id: el.project.id,
+            date: el.project.data.date,
+            images: el.project.data.images.map(
+              (img: { image: PrismicDocument }) => {
+                return {
+                  field: img.image.data,
+                  type: img.image.type,
+                };
+              }
+            ),
+            description: el.project.data.description,
+            title: el.project.data.title,
+            skills: el.project.data.skills,
+            "image-mobile": el.project.data["image-mobile"],
+            url: el.project.data.url ? el.project.data.url : null,
+          };
 
-    return projects;
+          return result;
+        }
+      );
+
+    return projects.filter((el) => el as Project); // Permet de filtrer les champs null
   }
 }
 export default ProjectsModule;
