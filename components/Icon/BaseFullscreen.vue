ef192f43ec808b6e71b493dd2328995cc9e7481c
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-11-10 15:04:24 +0100

    chore: update icons

--- components/Icon/BaseFullscreen.vue
+++ components/Icon/BaseFullscreen.vue
@@ -1,15 +1,7 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-  >
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-    />
+      d="M1.29688 5.16174V1.16174M1.29688 1.16174H5.29688M1.29688 1.16174L6.29688 6.16174M17.2969 5.16174V1.16174M17.2969 1.16174H13.2969M17.2969 1.16174L12.2969 6.16174M1.29688 13.1617V17.1617M1.29688 17.1617H5.29688M1.29688 17.1617L6.29688 12.1617M17.2969 17.1617L12.2969 12.1617M17.2969 17.1617V13.1617M17.2969 17.1617H13.2969"
+      stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 </template>
