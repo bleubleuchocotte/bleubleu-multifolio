13a4332600062db7254fb613c6c0e858044a8186
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-01-23 15:10:00 +0100

    refactor(prismic): replace hard-coded type with auto-generated type from Prismic

--- repository/modules/projects.ts
+++ repository/modules/projects.ts
@@ -1,89 +1,36 @@
-import type { FilledContentRelationshipField, LinkToMediaField } from "@prismicio/client";
-import type { ComponentMediaDuoDocumentData, ComponentMediaFullDocumentData, ProjetDocument, WebsiteDocument } from "@/prismicio-types";
-import type { MediaType, Project, SkillType } from "@/type/types";
+import type { Content, ContentRelationshipField } from "@prismicio/client";
 import PrismicFactory from "@/repository/factory";
+import type { ProjectWithId } from "~/types";
 
 class ProjectsModule extends PrismicFactory {
-	async getProjects() {
-		const { data } = await useAsyncData<WebsiteDocument>("GetAllProjects", () =>
-			this.client.getSingle("website", {
+	async getAllProjects() {
+		const { data } = await useAsyncData("GetAllProjects", () =>
+			this.client.getSingle<Content.WebsiteDocument>("website", {
 				graphQuery: `{
         website {
           projects {
             project {
 							...projectFields
-              skills {
-                skill
-              }
-              medias {
-                media {
-                  ...on component_media_full {
-                    ...component_media_fullFields
-                  }
-									...on component_media_duo {
-                    ...component_media_duoFields
-                  }
-                }
-              }
             }
           }
         }
       }`,
 			}));
 
-		const projectsPrismic = data.value?.data.projects ?? [];
+		const projectsFromPrismic = data.value?.data.projects ?? [];
 
-		const projects = projectsPrismic.map((el) => {
-			const projectPrismic = el.project as unknown as ProjetDocument; // ProjetDocument est un objet nested dans website
+		const projects = projectsFromPrismic.map((project) => {
+			const projectTyped = project.project as ContentRelationshipField<string, string, Content.ProjetDocumentData, "filled">;
 
-			const medias = projectPrismic.data.medias.map((el) => {
-				const mediaPrismic = el.media as FilledContentRelationshipField<"component_media_duo" | "component_media_full", "", ComponentMediaDuoDocumentData | ComponentMediaFullDocumentData>;
-
-				const result: MediaType = {
-					field: {},
-					type: mediaPrismic.type === "component_media_duo" ? "media-duo" : "media-full",
-					id: mediaPrismic.id,
-				};
-
-				switch (mediaPrismic.type) {
-					case "component_media_full":
-						result.field.principal = (mediaPrismic.data as ComponentMediaFullDocumentData).media_full as LinkToMediaField<"filled">;
-						break;
-					case "component_media_duo":
-						result.field.principal = (mediaPrismic.data as ComponentMediaDuoDocumentData).media_left as LinkToMediaField<"filled">;
-						result.field.secondary = (mediaPrismic.data as ComponentMediaDuoDocumentData).media_right as LinkToMediaField<"filled">;
-						break;
-
-					default:
-						break;
-				}
-
-				return result;
-			});
-
-			const skills = projectPrismic.data.skills.map((obj) => {
-				const result: SkillType = {
-					name: obj.skill ?? "",
-					id: useUID(),
-				};
-
-				return result;
-			});
-
-			const result: Project = {
-				id: projectPrismic.id,
-				date: projectPrismic.data.date ?? "",
-				medias,
-				description: projectPrismic.data.description,
-				title: projectPrismic.data.title ?? "",
-				skills,
-				url: projectPrismic.data.url ? projectPrismic.data.url : null,
-			};
-
-			return result;
+			if (projectTyped.data) {
+				return (Object.assign(projectTyped.data, { id: projectTyped.id }));
+			}
+			else {
+				return null;
+			}
 		});
 
-		return projects.filter((el): el is Project => el !== null); // Permet de filtrer les champs null
+		return projects.filter((el): el is ProjectWithId => el !== null); // Permet de filter les potentiels "undefined";
 	}
 }
 export default ProjectsModule;
