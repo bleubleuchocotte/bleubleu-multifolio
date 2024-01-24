859ab4e9bafbd863f765c67a2e60564e35d196f0
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-01-24 16:33:13 +0100

    feat: introduce preview

---
+++ pages/preview.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// The following code is mandatory for starting previews when entering this page
+usePrismicPreview();
+</script>
+
+<template>
+	<p>Loading Prismic preview...</p>
+</template>
