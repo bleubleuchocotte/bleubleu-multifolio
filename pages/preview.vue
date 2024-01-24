3a92d7537af4d53b4356ca528362d1fe63527e04
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-01-24 16:47:49 +0100

    chore: remove preview page and rename 404 for testing purpose

--- pages/preview.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-// The following code is mandatory for starting previews when entering this page
-usePrismicPreview();
-</script>
-
-<template>
-	<p>Loading Prismic preview...</p>
-</template>
