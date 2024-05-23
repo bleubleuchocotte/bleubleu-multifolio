c26390170f13f2bf0faeae9ef31abea6376377b6
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-05-23 18:43:58 +0200

    feat: disable devtool by default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	devtools: { enabled: false },
 	app: {
 		pageTransition: { name: "page", mode: "out-in" },
 	},
