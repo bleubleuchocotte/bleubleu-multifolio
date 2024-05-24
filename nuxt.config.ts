49aee075dfb8b0af2f6c2d0dce938af8f6c4d22c
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-05-24 15:28:11 +0200

    chore(image): update screens size; shift by 1 quality to have better result on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,5 +48,15 @@ export default defineNuxtConfig({
 				auto: false,
 			},
 		},
+
+		screens: {
+			"xs": 640,
+			"sm": 768,
+			"md": 1024,
+			"lg": 1280,
+			"xl": 1536,
+			"xxl": 1536,
+			"2xl": 1536,
+		},
 	},
 });
