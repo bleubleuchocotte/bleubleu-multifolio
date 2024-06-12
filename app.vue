3deb3ad0a01458307ad4dc2f5a193e3e81d78e2b
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-06-12 12:16:31 +0200

    feat: remove 16x16 favicon

--- app.vue
+++ app.vue
@@ -59,12 +59,6 @@ useServerHeadSafe({
 		{
 			rel: "icon",
 			type: "image/png",
-			sizes: "16x16",
-			href: options["seo-favicon"].url ?? `${request.origin}/default-favicon-16x16.png`,
-		},
-		{
-			rel: "icon",
-			type: "image/png",
 			sizes: "32x32",
 			href: options["seo-favicon"].url ?? `${request.origin}/default-favicon-32x32.png`,
 		},
